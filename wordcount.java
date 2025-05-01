ef4073fbb583404819e551ed2fcf653b354da655
Shailesh_Patil <147964994+Shailu2004@users.noreply.github.com>
2025-05-01 21:53:08 +0530

    Add files via upload

---
+++ wordcount.java
@@ -0,0 +1,44 @@
+package PackageDemo;
+
+import java.io.*;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+
+public class WordCount {
+  public static class MapperWC extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+    
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+      for (String w : value.toString().split(",")) {
+        word.set(w.trim().toUpperCase());
+        context.write(word, one);
+      }
+    }
+  }
+
+  public static class ReducerWC extends Reducer<Text, IntWritable, Text, IntWritable> {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) sum += val.get();
+      context.write(key, new IntWritable(sum));
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(WordCount.class);
+    job.setMapperClass(MapperWC.class);
+    job.setReducerClass(ReducerWC.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
